21d82c4ee01a6b32fae7a7932e7ca92be8b58531
Phillipj1514 <phillipjunior95@gmail.com>
2021-03-25 20:00:27 -0500

    fix dockerfile and docker image creation

--- front-ar-maps/src/components/Test.vue
+++ front-ar-maps/src/components/Test.vue
@@ -16,7 +16,7 @@ export default {
   },
   methods: {
     getMessage() {
-      const path = '/';
+      const path = '/api/test';
       axios.get(path)
         .then((res) => {
           this.msg = res.data;
