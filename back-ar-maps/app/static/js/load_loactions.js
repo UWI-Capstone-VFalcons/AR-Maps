173e742f2b875d6212c5f15258827db90df32a2f
Deandrew007 <deamoore07@gmail.com>
2021-04-16 01:32:32 -0500

    testing placing marker at home using js

---
+++ back-ar-maps/app/static/js/load_loactions.js
@@ -0,0 +1,38 @@
+window.onload = () => {
+    let places = staticLoadPlaces();
+    renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+   return [
+       {
+           name: 'Home Test',
+           location: {
+               lat: 18.0063554,
+               lng: -76.7423310,
+           }
+       },
+   ];
+}
+
+function renderPlaces(places) {
+   let scene = document.querySelector('a-scene');
+
+   places.forEach((place) => {
+       let latitude = place.location.lat;
+       let longitude = place.location.lng;
+
+       let model = document.createElement('a-entity');
+       model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+       model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+       model.setAttribute('rotation', '0 180 0');
+       model.setAttribute('animation-mixer', '');
+       model.setAttribute('scale', '0.5 0.5 0.5');
+
+       model.addEventListener('loaded', () => {
+           window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+       });
+
+       scene.appendChild(model);
+   });
+}
