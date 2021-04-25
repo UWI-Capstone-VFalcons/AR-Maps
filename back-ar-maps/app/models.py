0d52846a8fe85ac9376d18685817973569334bad
Phillipj1514 <phillipjunior95@gmail.com>
2021-04-25 01:04:43 -0500

    added map area and zone, path connections , fixed float datat type to numeric, added script to add data to databse

--- back-ar-maps/app/models.py
+++ back-ar-maps/app/models.py
@@ -14,10 +14,10 @@ class Building(db.Model):
     image_url = db.Column(db.Text)
     b_type = db.Column(db.String(20), nullable=False)
     info = db.Column(db.Text, nullable=False)
-    latitude = db.Column(db.Float(10,20), nullable=False)
-    longitude =  db.Column(db.Float(10, 20), nullable=False)
+    latitude = db.Column(db.Numeric(9,6), nullable=False)
+    longitude =  db.Column(db.Numeric(9,6), nullable=False)
 
-    def __init__(self, name, address1, address2, address3, image_url, b_type, info, latitude, longitude):
+    def __init__(self, name="unname", address1=None, address2=None, address3=None, image_url="default_building.jpg", b_type="Lecture Theatre", info="no imformation added", latitude=None, longitude=None):
         self.name = name
         self.address1 = address1
         self.address2 = address2
@@ -57,20 +57,14 @@ class Node(db.Model):
     __tablename__ = 'nodes'
     id = db.Column('node_id', db.Integer, primary_key=True)
     description = db.Column(db.Text)
-    latitude = db.Column(db.Float(10,20), nullable=False)
-    longitude =  db.Column(db.Float(10, 20), nullable=False)
-    latitude_2 = db.Column(db.Float(10,20))
-    longitude_2 = db.Column(db.Float(10,20))
-
-    def __init__(self, latitude, longitude, latitude_2, longitude_2):
-        self.latitude = latitude
-        self.longitude = longitude
-        self.latitude_2 = latitude_2
-        self.longitude_2 = longitude_2
+    latitude_1 = db.Column(db.Numeric(9,6), nullable=False)
+    longitude_1 =  db.Column(db.Numeric(9,6), nullable=False)
+    latitude_2 = db.Column(db.Numeric(9,6))
+    longitude_2 = db.Column(db.Numeric(9,6))
     
-    def __init__(self, latitude, longitude, latitude_2, longitude_2, description):
-        self.latitude = latitude
-        self.longitude = longitude
+    def __init__(self, latitude_1, longitude_1, latitude_2, longitude_2, description="no description"):
+        self.latitude_1 = latitude_1
+        self.longitude_1 = longitude_1
         self.latitude_2 = latitude_2
         self.longitude_2 = longitude_2
         self.description = description
@@ -85,32 +79,81 @@ class MapArea(db.Model):
     id = db.Column('map_area_id', db.Integer, primary_key=True)
     name = db.Column(db.String(200), unique=True, nullable=False)
     description = db.Column(db.Text)
-    point1 = db.Column(db.Integer, db.ForeignKey('nodes.node_id'), nullable=False)
-    point2 =  db.Column(db.Integer, db.ForeignKey('nodes.node_id'), nullable=False)
-    point3 =  db.Column(db.Integer, db.ForeignKey('nodes.node_id'), nullable=False)
-    point4 =  db.Column(db.Integer, db.ForeignKey('nodes.node_id'), nullable=False)
+    latitude_1 = db.Column(db.Numeric(9,6), nullable=False)
+    longitude_1 =  db.Column(db.Numeric(9,6), nullable=False)
 
-    def __init__(self, name="AREA", description = "Map AREA", point1=None, point2=None, point3=None, point4=None):
+    latitude_2 = db.Column(db.Numeric(9,6), nullable=False)
+    longitude_2 =  db.Column(db.Numeric(9,6), nullable=False)
+
+    latitude_3 = db.Column(db.Numeric(9,6), nullable=False)
+    longitude_3 =  db.Column(db.Numeric(9,6), nullable=False)
+
+    latitude_4 = db.Column(db.Numeric(9,6), nullable=False)
+    longitude_4 =  db.Column(db.Numeric(9,6), nullable=False)
+
+    def __init__(self, name="AREA", description = "Map AREA", latitude_1=None, longitude_1=None, latitude_2=None, longitude_2=None, latitude_3=None, longitude_3=None, latitude_4=None, longitude_4=None):
         self.name = name
         self.description = description
-        self.point1 = point1
-        self.point2 = point2
-        self.point3 = point3
-        self.point4 = point4
+        self.latitude_1 = latitude_1
+        self.longitude_1 = longitude_1
+        self.latitude_2 = latitude_2
+        self.longitude_2 = longitude_2
+        self.latitude_3 = latitude_3
+        self.longitude_3 = longitude_3
+        self.latitude_4 = latitude_4
+        self.longitude_4 = longitude_4
 
     def __repr__(self):
-        return '<Map_Areas %r>' % self.id
+        return '<Map_Area %r>' % self.id
+
+# The model used to save the gps coordinates for the zones that 
+# are in an area  
+class MapZone(db.Model):
+    __tablename__ = 'map_zones'
+    id = db.Column('map_zone_id', db.Integer, primary_key=True)
+    name = db.Column(db.String(200), unique=True, nullable=False)
+    map_area = db.Column(db.Integer, db.ForeignKey('map_areas.map_area_id'), nullable=False)
+
+    latitude_1 = db.Column(db.Numeric(9,6), nullable=False)
+    longitude_1 =  db.Column(db.Numeric(9,6), nullable=False)
+
+    latitude_2 = db.Column(db.Numeric(9,6), nullable=False)
+    longitude_2 =  db.Column(db.Numeric(9,6), nullable=False)
+
+    latitude_3 = db.Column(db.Numeric(9,6), nullable=False)
+    longitude_3 =  db.Column(db.Numeric(9,6), nullable=False)
+
+    latitude_4 = db.Column(db.Numeric(9,6), nullable=False)
+    longitude_4 =  db.Column(db.Numeric(9,6), nullable=False)
+
+    def __init__(self, name="Zone", map_area=None, latitude_1=None, longitude_1=None, latitude_2=None, longitude_2=None, latitude_3=None, longitude_3=None, latitude_4=None, longitude_4=None):
+        self.name = name
+        self.map_area = map_area
+        self.latitude_1 = latitude_1
+        self.longitude_1 = longitude_1
+        self.latitude_2 = latitude_2
+        self.longitude_2 = longitude_2
+        self.latitude_3 = latitude_3
+        self.longitude_3 = longitude_3
+        self.latitude_4 = latitude_4
+        self.longitude_4 = longitude_4
+
+    def __repr__(self):
+        return '<Map_Zone %r>' % self.id
 
 # The model saves all the paths that are recorded in the scitech area
 class Path(db.Model):
     __tablename__ = 'paths'
     id = db.Column('path_id', db.Integer, primary_key=True)
+    name = db.Column(db.String(200), unique=True, nullable=False)
+    description = db.Column(db.Text)
     map_area = db.Column(db.Integer, db.ForeignKey('map_areas.map_area_id'), nullable=False)
     start = db.Column(db.Integer, db.ForeignKey('nodes.node_id'), nullable=False)
     end =  db.Column(db.Integer, db.ForeignKey('nodes.node_id'), nullable=False)
-    length = db.Column(db.Float(10, 20), nullable=False)
+    length = db.Column(db.Numeric(9,6))
 
-    def __init__(self, start, end, length, map_area):
+    def __init__(self, name="unname", start=None, end=None, length=None, map_area=None):
+        self.name = name
         self.start = start
         self.end = end
         self.length = length
@@ -119,16 +162,29 @@ class Path(db.Model):
     def __repr__(self):
         return '<Path %r>' % self.id
 
+class Path_Connection(db.Model):
+    __tablename__ = 'path_connections'
+    id = db.Column('path_connection_id', db.Integer, primary_key=True)
+    path1 = db.Column(db.Integer, db.ForeignKey('paths.path_id'), nullable=False)
+    path2 = db.Column(db.Integer, db.ForeignKey('paths.path_id'), nullable=False)
+
+    def __init__(self, path1=None, path2=None):
+        self.path1 = path1
+        self.path2 = path2
+
+    def __repr__(self):
+        return '<Path_Connection %r>' % self.id
+
 # This model defines all the starting point locations
 # to the sci tech area
 class Starting_Point(db.Model):
     __tablename__ = 'starting_points'
     id = db.Column('sp_id', db.Integer, primary_key=True)
     name = db.Column(db.String(200), unique=True, nullable=False)
-    latitude = db.Column(db.Float(10,20), nullable=False)
-    longitude =  db.Column(db.Float(10, 20), nullable=False)
+    latitude = db.Column(db.Numeric(9,6), nullable=False)
+    longitude =  db.Column(db.Numeric(9,6), nullable=False)
 
-    def __init__(self, name, latitude, longitude):
+    def __init__(self, name="unname", latitude=None, longitude=None):
         self.name = name
         self.latitude = latitude
         self.longitude = longitude
@@ -143,12 +199,16 @@ class OD_Objects(db.Model):
     id = db.Column('object_id', db.Integer, primary_key=True)
     name = db.Column(db.String(200), unique=True, nullable=False)
     object_name = db.Column(db.String(200), unique=True, nullable=False)
-    latitude = db.Column(db.Float(10,20), nullable=False)
-    longitude =  db.Column(db.Float(10, 20), nullable=False)
+    map_zone = db.Column(db.Integer, db.ForeignKey('map_zones.map_zone_id'))
+    building_id = db.Column(db.Integer, db.ForeignKey('buildings.building_id'))
+    latitude = db.Column(db.Numeric(9,6), nullable=False)
+    longitude =  db.Column(db.Numeric(9,6), nullable=False)
 
-    def __init__(self, name, object_name, latitude, longitude):
+    def __init__(self, name="unname", object_name="unname", map_zone=None, building_id=None, latitude=None, longitude=None):
         self.name = name
         self.object_name = object_name
+        self.map_zone = map_zone
+        self.building_id = building_id
         self.latitude = latitude
         self.longitude = longitude
 
---
+++ back-ar-maps/populate_db.py
@@ -0,0 +1,318 @@
+from app import db
+from app.models import *
+
+# add default buildings
+ 
+b1 = Building(
+    name='Science and Technology Library',
+    address1="2742+46", 
+    address2="Kingston, Jamaica", 
+    b_type="Library", 
+    info="The main Library of the Sci- Tech faculty",
+    latitude=18.005339,
+    longitude=-76.749472)
+
+b2 = Building(
+    name='Science Lecture Theatre 2',
+    address1="2742+34", 
+    address2="Kingston, Jamaica", 
+    b_type="Lecture Theatre", 
+    info="SLT 2",
+    latitude=18.005164,
+    longitude=-76.749748)
+
+b3 = Building(
+    name='Science Lecture Theatre 1',
+    address1="2742+32", 
+    address2="Kingston, Jamaica", 
+    b_type="Lecture Theatre", 
+    info="SLT 1",
+    latitude=18.005172,
+    longitude=-76.749948)
+
+b4 = Building(
+    name='Science and Technology Faculty Office',
+    address1="2742+32", 
+    address2="Kingston, Jamaica", 
+    b_type="Lecture Theatre", 
+    info="The faculty office and Deans office ",
+    latitude=18.005356,
+    longitude=-76.749883)
+
+db.session.add(b1)
+db.session.add(b2)
+db.session.add(b3)
+db.session.add(b4)
+
+# commit the insertions to the database
+db.session.commit()
+
+# add events
+
+# Add Map Area
+ma1 = MapArea(
+    name="Science & Technology Faculty",
+    description="The science faculty of the Universiy of the west Indies",
+    latitude_1= 18.003364,
+    longitude_1= -76.749843,
+    latitude_2= 18.005936,
+    longitude_2= 18.005936, 
+    latitude_3= 18.006836,
+    longitude_3= -76.749104,
+    latitude_4= 18.004963,
+    longitude_4= -76.748221)
+
+db.session.add(ma1)
+
+# commit the insertions to the database
+db.session.commit()
+
+# add Map Zones
+mz1 = MapZone(
+    name="Zone A",
+    map_area=1,
+    latitude_1= 18.005706,
+    longitude_1= -76.748579,
+    latitude_2= 18.005494,
+    longitude_2= -76.748539 ,
+    latitude_3= 18.005327,
+    longitude_3= -76.748749,
+    latitude_4= 18.005529284218035,
+    longitude_4=-76.74892187118532)
+
+mz2 = MapZone(
+    name="Zone B",
+    map_area=1,
+    latitude_1= 18.005529284218035,
+    longitude_1=-76.74892187118532,
+    latitude_2= 18.005327,
+    longitude_2= -76.748749,
+    latitude_3= 18.005167062810408,
+    longitude_3= -76.74905598163606 ,
+    latitude_4= 18.005406843262104,
+    longitude_4= -76.74925446510316)
+
+mz3 = MapZone(
+    name="Zone C",
+    map_area=1,
+    latitude_1= 18.005406843262104,
+    longitude_1= -76.74925446510316,
+    latitude_2= 18.005167062810408,
+    longitude_2= -76.74905598163606,
+    latitude_3= 18.005065,
+    longitude_3= -76.749454,
+    latitude_4= 18.00524358852193,
+    longitude_4= -76.74952805042268)
+
+mz4 = MapZone(
+    name="Zone D",
+    map_area=1,
+    latitude_1= 18.00524358852193,
+    longitude_1= -76.74952805042268,
+    latitude_2= 18.005065,
+    longitude_2= -76.749454,
+    latitude_3= 18.004976,
+    longitude_3= -76.749713,
+    latitude_4= 18.005110943934206,
+    longitude_4= -76.74973189830781)
+
+db.session.add(mz1)
+db.session.add(mz2)
+db.session.add(mz3)
+db.session.add(mz4)
+
+# commit the insertions to the database
+db.session.commit()
+
+# add nodes
+n1 = Node(
+    latitude_1=18.005656,
+    longitude_1=-76.748537,
+    latitude_2= 18.005612,
+    longitude_2= -76.748541,
+    description= "sci-tech entrance")
+
+n2 = Node(
+    latitude_1=18.005330317621468,
+    longitude_1=-76.74919009208679,
+    latitude_2= 18.00526399537271,
+    longitude_2= -76.74915790557861,
+    description= "sci-tech entrance")
+
+n3 = Node(
+    latitude_1=18.005046,
+    longitude_1=-76.749713,
+    latitude_2= 18.005015,
+    longitude_2= -76.749696,
+    description= "Miss.Jackie Stall")  
+
+n4 = Node(
+    latitude_1=18.005013,
+    longitude_1=-76.749707,
+    latitude_2= 18.005002,
+    longitude_2= -76.749733,
+    description= "Miss.Jackie Stall")  
+
+n4 = Node(
+    latitude_1=18.005482,
+    longitude_1=-76.749959,
+    latitude_2= 18.005465,
+    longitude_2= -76.750019,
+    description= "Faculty Office")  
+
+n5 = Node(
+    latitude_1=18.004602,
+    longitude_1=-76.749443,
+    latitude_2= 18.005002,
+    longitude_2= -76.749733,
+    description= "Mathematics department")  
+
+db.session.add(n1)
+db.session.add(n2)
+db.session.add(n3)
+db.session.add(n4)
+db.session.add(n5)
+
+# commit the insertions to the database
+db.session.commit()
+
+# add paths
+p1 = Path(
+    name="sci-tech Juici Patty entrance",
+    start=1,
+    end=2,
+    map_area=1)
+
+p2 = Path(
+    name="sci-tech library",
+    start=2,
+    end=3,
+    map_area=1)
+
+p3 = Path(
+    name="sci-tech faculty office",
+    start=3,
+    end=4,
+    map_area=1)
+
+p4 = Path(
+    name="sci-tech mathematics department",
+    start=3,
+    end=5,
+    map_area=1)
+
+db.session.add(p1)
+db.session.add(p2)
+db.session.add(p3)
+db.session.add(p4)
+
+# commit the insertions to the database
+db.session.commit()
+
+# add path connections
+pc1 = Path_Connection(
+    path1=1,
+    path2=2)
+
+pc2 = Path_Connection(
+    path1=2,
+    path2=3)
+
+pc3 = Path_Connection(
+    path1=2,
+    path2=4)
+
+db.session.add(pc1)
+db.session.add(pc2)
+db.session.add(pc3)
+
+# commit the insertions to the database
+db.session.commit()
+
+# Add starting points
+sp = Starting_Point(
+    name= "Juici Patty Entrance", 
+    latitude= 18.005642, 
+    longitude= -76.748532)
+
+db.session.add(sp)
+
+# commit the insertions to the database
+db.session.commit()
+
+# add OD objects
+odo1 = OD_Objects(
+    name="Aunty Jackie Stall", 
+    object_name="18.0050609_-76.7496662_auntyjackie", 
+    map_zone=4,
+    latitude=18.0050609, 
+    longitude=-76.7496662)
+
+odo2 = OD_Objects(
+    name="Aunty Jackie Bench", 
+    object_name="18.0050864_-76.7496363_bench", 
+    map_zone=4,
+    latitude=18.0050864, 
+    longitude=-76.7496363)
+
+odo3 = OD_Objects(
+    name="Sci-Tech Library Stall", 
+    object_name="18.0051667_-76.74944412_librarystall", 
+    map_zone=3,
+    latitude=18.0051667, 
+    longitude=-76.74944412)
+
+odo4 = OD_Objects(
+    name="Science Lecture Theatre 1 Door", 
+    object_name="18.0051836_-76.7497262_pclt1", 
+    building_id=3,
+    latitude=18.0051836, 
+    longitude=-76.7497262)
+
+odo5 = OD_Objects(
+    name="Sci-Tech entrance recycle bin", 
+    object_name="18.0053245_-76.7486424_recyclebin", 
+    map_zone=1,
+    latitude=18.0053245, 
+    longitude=-76.7486424)
+
+odo6 = OD_Objects(
+    name="Sci-Tech entrance Sign", 
+    object_name="18.0053530_-76.7490375_signatentrancetoscitech", 
+    map_zone=1,
+    latitude=18.0053530, 
+    longitude=-76.7490375)
+
+odo7 = OD_Objects(
+    name="Sci-Tech Library Sign", 
+    object_name="18.0053644_-76.7493880_librarysign", 
+    map_zone=3,
+    latitude=18.0053644, 
+    longitude=-76.7493880)
+
+odo8 = OD_Objects(
+    name="Sci-Tech Physics Department Sign", 
+    object_name="18.0053657_-76.7491452_physicssign", 
+    map_zone=2,
+    latitude=18.0053657, 
+    longitude=-76.7491452)
+
+odo9 = OD_Objects(
+    name="Sci-Tech blue sign", 
+    object_name="18.0055634_-76.7484079_scitechsign", 
+    map_zone=2,
+    latitude=18.0055634, 
+    longitude=-76.7484079)
+
+db.session.add(odo1)
+db.session.add(odo2)
+db.session.add(odo3)
+db.session.add(odo4)
+db.session.add(odo5)
+db.session.add(odo6)
+db.session.add(odo7)
+db.session.add(odo8)
+db.session.add(odo9)
+
+# commit the insertions to the database
+db.session.commit()
