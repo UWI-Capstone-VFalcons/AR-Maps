a95ec9d7a21db3bc56d562e52449b7a42c0bb1ac
Phillipj1514 <phillipjunior95@gmail.com>
2021-04-30 08:41:33 -0500

    added paths to the google map and circle arounnd the user to show accuracy

--- back-ar-maps/test_ground.py
+++ back-ar-maps/test_ground.py
@@ -3,9 +3,9 @@ from app.helper import *
 from app.views import *
 
 if __name__ == '__main__':
-   # testBuilding = Building.query.get(3)
-   # a = generateShortestRoute(1, testBuilding,1)
-   # print(a)
-
-   a = dist((17.917338, -76.192295), (18.363768, -78.333022))
+   testBuilding = Building.query.get(3)
+   a = generateShortestRoute(1, testBuilding,1)
    print(a)
+
+   # a = dist((17.917338, -76.192295), (18.363768, -78.333022))
+   # print(a)
