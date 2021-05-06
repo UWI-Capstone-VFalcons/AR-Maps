45d711668a8cdcdb26ddb894006d2ec732afbf92
Deandrew007 <deamoore07@gmail.com>
2021-05-05 23:05:47 -0500

    testing changes

--- back-ar-maps/app/static/js/render_ar.js
+++ back-ar-maps/app/static/js/render_ar.js
@@ -1,7 +1,9 @@
 window.onload = () => {
     let places = dynamicallyLoadPlaces();
-    renderPlaces(places);
+  //  renderPlaces(places);
 };
+
+//json returns root- has indexes of paths, postion- long and lat, meta - distance and time
 
 // getting places from APIs
 function dynamicallyLoadPlaces() {
@@ -31,38 +33,38 @@ function dynamicallyLoadPlaces() {
 
 }
 
-window.onload = () => {
-    const scene = document.querySelector('a-scene');
+// window.onload = () => {
+//     const scene = document.querySelector('a-scene');
 
-    // first get current user location
-    return navigator.geolocation.getCurrentPosition(function (position) {
+//     // first get current user location
+//     return navigator.geolocation.getCurrentPosition(function (position) {
 
-        // than use it to load from remote APIs some places nearby
-        loadPlaces(position.coords)
-            .then((places) => {
-                places.forEach((place) => {
-                    const latitude = place.location.lat;
-                    const longitude = place.location.lng;
+//         // than use it to load from remote APIs some places nearby
+//         loadPlaces(position.coords)
+//             .then((places) => {
+//                 places.forEach((place) => {
+//                     const latitude = place.location.lat;
+//                     const longitude = place.location.lng;
 
-                    // add place name
-                    const placeText = document.createElement('a-link');
-                    placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-                    placeText.setAttribute('title', place.name);
-                    placeText.setAttribute('scale', '15 15 15');
+//                     // add place name
+//                     const placeText = document.createElement('a-link');
+//                     placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+//                     placeText.setAttribute('title', place.name);
+//                     placeText.setAttribute('scale', '15 15 15');
                     
-                    placeText.addEventListener('loaded', () => {
-                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-                    });
+//                     placeText.addEventListener('loaded', () => {
+//                         window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+//                     });
 
-                    scene.appendChild(placeText);
-                });
-            })
-    },
-        (err) => console.error('Error in retrieving position', err),
-        {
-            enableHighAccuracy: true,
-            maximumAge: 0,
-            timeout: 27000,
-        }
-    );
-};
+//                     scene.appendChild(placeText);
+//                 });
+//             })
+//     },
+//         (err) => console.error('Error in retrieving position', err),
+//         {
+//             enableHighAccuracy: true,
+//             maximumAge: 0,
+//             timeout: 27000,
+//         }
+//     );
+// };
